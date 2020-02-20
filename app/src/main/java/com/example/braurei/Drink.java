1be88bcf8420b0e4b3a7f1e75c538b3790811aae
Majkl <majkl.andracic@gmail.com>
2020-02-20 19:20:47 +0100

    Initial

--- app/src/main/java/com/example/braurei/Drink.java
+++ app/src/main/java/com/example/braurei/Drink.java
@@ -6,9 +6,26 @@ public class Drink {
     private int imageResourceId;
 
     public static final Drink[] drinks = {
-            new Drink("Schönbuch Helles", "Schönes bier", R.drawable.schoenbuch_helles);
-            new Drink("Schönbuch Weiss","Schönes weizen",R.drawable.schoenbuch_weizen);
-    };
+            new Drink("Schönbuch Helles", "Schönes bier", R.drawable.schoenbuch_helles),
+            new Drink("Schönbuch Weiss","Schönes weizen",R.drawable.schoenbuch_weizen)
+     };
 
-    private
+    public Drink(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+    
 }
