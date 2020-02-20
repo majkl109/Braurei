fc90f1a2649819a8a6bbb4d56ec1730e182f4056
Majkl <majkl.andracic@gmail.com>
2020-02-20 19:17:16 +0100

    Initial

---
+++ app/src/main/java/com/example/braurei/Drink.java
@@ -0,0 +1,14 @@
+package com.example.braurei;
+
+public class Drink {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    public static final Drink[] drinks = {
+            new Drink("Schönbuch Helles", "Schönes bier", R.drawable.schoenbuch_helles);
+            new Drink("Schönbuch Weiss","Schönes weizen",R.drawable.schoenbuch_weizen);
+    };
+
+    private
+}
