4b39762abed839173f5498d1291c1742644f91b9
Majkl <majkl.andracic@gmail.com>
2020-02-21 22:34:11 +0100

    Adapter

--- app/src/main/java/com/example/braurei/CaptionedImagesAdapter.java
+++ app/src/main/java/com/example/braurei/CaptionedImagesAdapter.java
@@ -1,11 +1,25 @@
 package com.example.braurei;
 
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
 
+    private String[] captions;
+    private int[] imageId;
+
     public static class ViewHolder extends RecyclerView.ViewHolder{
 
+        private CardView cardView;
+
+        public ViewHolder(CardView v){
+            super(v);
+            cardView = v;
+        }
     }
 
+    public CaptionedImagesAdapter(String[] captions, int[] imageId) {
+        this.captions = captions;
+        this.imageId = imageId;
+    }
 }
